0de6feee7d3ac7ea9fc5585c0976d02afbc6e369
Harris Schrick <schrickh+github@gmail.com>
2022-01-15 22:23:11 -0600

    test

--- palindrome-number.py
+++ palindrome-number.py
@@ -5,9 +5,9 @@ class Solution:
         number_length = len(converted_x)
         
         for index, item in enumerate(converted_x):
-            print("front index is: " + str(index) + " and the number is " + item)
+            print("front index is: " + str(index) + " and the number is: " + item)
             
-            print("back index is: " + str(len(converted_x) - index) + " and the number is " + converted_x[(len(converted_x) - 1) - index])
+            print("back index is: " + str(len(converted_x) - index) + " and the number is: " + converted_x[(len(converted_x) - 1) - index])
             
             if item != converted_x[(len(converted_x) - 1) - index]:
                 return False
